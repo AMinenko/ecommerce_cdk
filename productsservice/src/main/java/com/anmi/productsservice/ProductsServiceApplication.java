5d92ae98e07cef886eb80179fb808358c6f68021
Andriy Minenko <mr.minenko@Andriys-MacBook-Pro.local>
2025-09-26 16:27:24 +0200

    Trigger build

--- productsservice/src/main/java/com/anmi/productsservice/ProductsServiceApplication.java
+++ productsservice/src/main/java/com/anmi/productsservice/ProductsServiceApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductsServiceApplication {
 
-    /**
-     * Just to trigger the build :)
-     * @param args
-     */
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
 	}
